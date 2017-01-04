3542fdd1c60530a9aa6e382c2e656175aa9756dc
melerz <guy.meler@mail.huji.ac.il>
2017-01-04 13:10:17 +0200

    commit

--- app/mod_auth/views.py
+++ app/mod_auth/views.py
@@ -1,1 +1,13 @@
-#kernel2
+# Import flask dependencies
+from flask import Blueprint, request, render_template, \
+    flash, g, session, redirect, url_for
+
+# Define the blueprint: 'auth', set its url prefix: app.url/auth
+mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+# Set the route and accepted methods
+@mod_auth.route('/signin/', methods=['GET', 'POST'])
+def signin():
+    print("tahat")
+    return render_template("auth/signin.html")
